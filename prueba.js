ac0d94f3210bba9a883c4db080a597be73d37e1a
Rafael Cosquillo <cosquillo00@gmail.com>
2025-12-09 09:09:35 +0100

    Eliminacion de prueba.js

--- prueba.js
+++
@@ -1,9 +0,0 @@
-const { Builder } = require('selenium-webdriver');
-require('chromedriver'); // <- IMPORTANTE
-
-(async function testChrome() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://www.google.com');
-    console.log('Chrome abierto 🚀');
-    await driver.quit();
-})();
